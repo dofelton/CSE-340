e97c12fd81a2f5ef146536960290d6a1efec78bc
dofelton <dofelton@gmail.com>
2024-05-02 16:00:38 -0500

    5/2.2

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -10,3 +10,36 @@ SET
 	account_type = 'admin'
 WHERE
 	account_id = 1;
+
+-- 3
+DELETE
+FROM
+	account
+WHERE
+	account_id = 1;
+
+--4
+UPDATE
+	public.inventory
+SET
+	inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE
+	inv_id = 10;
+
+--5
+SELECT 
+	inv_make,
+	inv_model
+FROM
+	public.inventory
+LEFT JOIN public.classification
+	ON inventory.classification_id = classification.classification_id
+WHERE
+	inventory.classification_id = 2;
+
+--6
+UPDATE
+	public.inventory
+SET
+	inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
