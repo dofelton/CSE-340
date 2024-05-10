26eff1d17685ac1633bb28df2d48e9a086d0fa1b
dofelton <dofelton@gmail.com>
2024-05-09 20:29:31 -0500

    W3 build

--- server.js
+++ server.js
@@ -29,11 +29,15 @@ app.use(static)
 app.get("/", baseController.buildHome)
 // Inventory routes
 app.use("/inv", inventoryRoute)
+// File Not Found Route - last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
-/* *******************
- * Express Error Handler
- * Place after all other mmiddleware
- * ************************* */
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
