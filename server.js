ff4d1f2a7a02f210a642056f0d58f9a692431778
Don Felton <dofelton@gmail.com>
2024-05-13 16:22:42 -0500

    5/13

--- server.js
+++ server.js
@@ -12,7 +12,31 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("../utilities/")
+const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+
+/* *********************
+ * Middleware
+ * ********************** */
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  RESAVE: true,
+  saveUninitialized: true,
+  name: 'sesionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
