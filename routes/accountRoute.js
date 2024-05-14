2be4e32ff462a2865ae9ae3cd894fa50b3b8f765
Don Felton <dofelton@gmail.com>
2024-05-14 09:38:07 -0500

    5/13

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,6 +4,6 @@ const router = new express.Router()
 const accountsController = require("../utilities/index")
 
 // Route to Login View
-router.get("/type/:login", accountController.);
+// router.get("/type/:login", accountController.);
 
 module.exports = router;
