4971b50b0f9af731b20a2db76e1228289b68d8af
Don Felton <dofelton@gmail.com>
2024-05-16 20:57:24 -0500

    Testing

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,15 +1,21 @@
-// Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const accountController = require("../controllers/accountController")
-const Util = require("../utilities")
+// // Needed Resources 
+// const express = require("express")
+// const router = new express.Router() 
+// const accountController = require("../controllers/accountController")
+// const Util = require("../utilities")
+// const regValidate = require('../utilities/account-validation')
 
-// Route to build login view
-router.get("/login", Util.handleErrors(accountController.buildLogin))
+// // Route to build login view
+// router.get("/login", Util.handleErrors(accountController.buildLogin))
 
-// Route to build registration view
-router.get("/register", Util.handleErrors(accountController.buildRegister))
+// // Route to build registration view
+// router.get("/register", Util.handleErrors(accountController.buildRegister))
 
-// Route for registration submission
-router.post('/register', Util.handleErrors(accountController.registerAccount))
-module.exports = router;
+// // Route for registration submission
+// router.post(
+//     "/register",
+//     regValidate.registrationRules(),
+//     regValidate.checkRegData,
+//     utilities.handleErrors(accountController.registerAccount))
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -10,37 +10,37 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities/")
-const session = require("express-session")
-const pool = require('./database/')
-const accountRoute = require("./routes/accountRoute")
-const bodyParser = require("body-parser")
+// const baseController = require("./controllers/baseController")
+// const inventoryRoute = require("./routes/inventoryRoute")
+// const utilities = require("./utilities/")
+// const session = require("express-session")
+// const pool = require('./database/')
+// const accountRoute = require("./routes/accountRoute")
+// const bodyParser = require("body-parser")
 
-/* *********************
- * Middleware
- * ********************** */
-app.use(session({
-  store: new (require('connect-pg-simple')(session))({
-    createTableIfMissing: true,
-    pool,
-  }),
-  secret: process.env.SESSION_SECRET,
-  RESAVE: true,
-  saveUninitialized: true,
-  name: 'sesionId',
-}))
+// /* *********************
+//  * Middleware
+//  * ********************** */
+// app.use(session({
+//   store: new (require('connect-pg-simple')(session))({
+//     createTableIfMissing: true,
+//     pool,
+//   }),
+//   secret: process.env.SESSION_SECRET,
+//   RESAVE: true,
+//   saveUninitialized: true,
+//   name: 'sesionId',
+// }))
 
-// Express Messages Middleware
-app.use(require('connect-flash')())
-app.use(function(req, res, next){
-  res.locals.messages = require('express-messages')(req, res)
-  next()
-})
+// // Express Messages Middleware
+// app.use(require('connect-flash')())
+// app.use(function(req, res, next){
+//   res.locals.messages = require('express-messages')(req, res)
+//   next()
+// })
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ exttended: true }))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ exttended: true }))
 
 /* ***********************
  * View Engine and Templates
@@ -54,30 +54,32 @@ app.set("layout", "./layouts/layout")
  *************************/
 app.use(static)
 // index route
-app.get("/", utilities.handleErrors(baseController.buildHome))
-// Inventory routes
-app.use("/inv", inventoryRoute)
-// File Not Found Route - last route in list
-app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
 })
-// Account routes
-app.use("/account", require("./routes/accountRoute"))
+// // Inventory routes
+// app.use("/inv", inventoryRoute)
+// // File Not Found Route - last route in list
+// app.use(async (req, res, next) => {
+//   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+// })
+// // Account routes
+// app.use("/account", require("./routes/accountRoute"))
 
-/* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
-app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
-  res.render("errors/error", {
-    title: err.status || 'Server Error',
-    message,
-    nav
-  })
-})
+// /* ***********************
+// * Express Error Handler
+// * Place after all other middleware
+// *************************/
+// app.use(async (err, req, res, next) => {
+//   let nav = await utilities.getNav()
+//   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+//   if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+//   res.render("errors/error", {
+//     title: err.status || 'Server Error',
+//     message,
+//     nav
+//   })
+// })
 
 /* ***********************
  * Local Server Information
