c4e78847310479d1f27c0afbc462c8a265664707
Don Felton <dofelton@gmail.com>
2024-05-28 19:25:05 -0500

    Assign4

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -58,7 +58,7 @@ async function addClassification(classification_name) {
 async function addInventory(classification_name) {
   try {
       console.log('we are in the addInventory method')
-      const sql = "INSERT INTO inventory (classification_id, inv_make, inv_model, inv_year, inv_price, inv_description, inv_milage, inv_color) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *"
+      const sql = "INSERT INTO inventory (classification_id, inv_make, inv_model, inv_year, inv_price, inv_description, inv_milage, inv_color, inv_image, inv_thumbnail) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, '/images/vehicles/no-image.png', '/images/vehicles/no-image-tn.png') RETURNING *"
       return await pool.query(sql, [classification_id, inv_make, inv_model, inv_year, inv_price, inv_description, inv_milage, inv_color])
     } catch (error) {
       return error.message
